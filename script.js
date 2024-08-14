e0ae7765b0322199b1dee3ba60e5b0ab4d6c07df
sukjo <sukjoanne@gmail.com>
2024-08-14 11:29:01 -0400

    3d scene setup with shadow interaction

--- script.js
+++ script.js
@@ -1,39 +1,144 @@
-$(function () {
-  let dragging = false;
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+import { AsciiEffect } from "three/addons/effects/AsciiEffect.js";
 
-  let cc = $("#column-container");
-  cc.mousedown(function () {
-    dragging = true;
+let scene, renderer, camera, light, gui, effect;
+
+function addLight(scene) {
+  light = new THREE.DirectionalLight(0xffffff, 1);
+  //     const light = new THREE.SpotLight(0xffffff, 1);
+  //   const light = new THREE.PointLight(0xffffff, 1);
+
+  light.castShadow = true;
+  const sBound = 20; // doesn't have to be too wide as long as the camera pos remains steady (no zoom)
+  light.shadow.camera.left = -sBound;
+  light.shadow.camera.right = sBound;
+  light.shadow.camera.top = sBound;
+  light.shadow.camera.bottom = -sBound;
+  light.shadow.radius = 6;
+  light.shadow.blurSamples = 25;
+  scene.add(light);
+}
+
+function addModel(scene) {
+  const modelGeo = new THREE.BoxGeometry(2, 2, 2);
+  const modelMat = new THREE.MeshStandardMaterial({
+    color: 0xf2f2f2,
+    colorWrite: false,
   });
-  cc.mouseup(function () {
-    dragging = false;
+  const model = new THREE.Mesh(modelGeo, modelMat);
+  model.castShadow = true;
+  model.position.y = 2;
+  scene.add(model);
+}
+
+function addPlane(scene) {
+  const planeGeo = new THREE.PlaneGeometry(20, 20);
+  const planeMat = new THREE.ShadowMaterial({
+    opacity: 0.5,
   });
-  cc.mousemove(function (e) {
-    if (dragging) {
-      let mouseX = e.pageX;
-      let mouseY = e.pageY;
+  const plane = new THREE.Mesh(planeGeo, planeMat);
+  plane.rotation.x = -Math.PI / 2;
+  plane.receiveShadow = true;
+  scene.add(plane);
+}
 
-      let rad = Math.atan2(mouseX - 10, mouseY - 10);
-      let deg = rad * (180 / Math.PI) * -1 + 90;
+function addGUI() {
+  gui = new GUI();
+  const lightFolder = gui.addFolder("light");
+  lightFolder.add(light.shadow, "radius", 0, 50, 1).onChange(function (val) {
+    light.shadow.radius = val;
+  });
+  lightFolder
+    .add(light.shadow, "blurSamples", 1, 25, 1)
+    .onChange(function (val) {
+      light.shadow.blurSamples = val;
+    });
+}
 
-      cc.css("-moz-transform", "rotate(" + deg + "deg)");
-      cc.css("-moz-transform-origin", "0% 40%");
-      cc.css("-webkit-transform", "rotate(" + deg + "deg)");
-      cc.css("-webkit-transform-origin", "0% 40%");
-      cc.css("-o-transform", "rotate(" + deg + "deg)");
-      cc.css("-o-transform-origin", "0% 40%");
-      cc.css("-ms-transform", "rotate(" + deg + "deg)");
-      cc.css("-ms-transform-origin", "0% 40%");
-    }
+function addHelpers(scene) {
+  const helper = new THREE.DirectionalLightHelper(light, 3, 0x000000);
+  //     const helper = new THREE.SpotLightHelper(light, 3, 0x000000);
+  //   const helper = new THREE.PointLightHelper(light, 3, 0x000000);
+  scene.add(helper);
+
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper);
+}
+
+function onWindowResize() {
+  const aspect = window.innerWidth / window.innerHeight;
+  camera.left = -10 * aspect;
+  camera.right = 10 * aspect;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  //   effect.setSize(window.innerWidth, window.innerHeight);
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    SCENE                                   */
+/* -------------------------------------------------------------------------- */
+
+function initScene() {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xf2f2f2);
+
+  const cBound = 15;
+  camera = new THREE.OrthographicCamera(
+    -cBound,
+    cBound,
+    cBound,
+    -cBound,
+    0.1,
+    100
+  );
+  camera.position.set(0, 10, 0);
+  camera.lookAt(0, 0, 0);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.VSMShadowMap;
+  //   renderer.shadowMap.type = THREE.PCFShadowMap;
+  //   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+  effect = new AsciiEffect(renderer, "#OX. ", { invert: true });
+  effect.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+  //   document.body.appendChild(effect.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  //   const controls = new OrbitControls(camera, effect.domElement);
+
+  addLight(scene);
+  addModel(scene);
+  addPlane(scene);
+  //   addHelpers(scene);
+  //   addGUI();
+
+  document.addEventListener("mousemove", (event) => {
+    const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+    light.position.set(mouseX * 10, 5, mouseY * -10);
+    light.lookAt(0, 0, 0);
   });
 
-  let dummy = 180;
+  window.addEventListener("resize", () => {
+    onWindowResize();
+  });
+}
 
-  //   $("#reunification-highway").css({
-  //     "-webkit-transform": "scale(" + dummy + ")",
-  //     "-moz-transform": "scale(" + dummy + ")",
-  //     "-ms-transform": "scale(" + dummy + ")",
-  //     "-o-transform": "scale(" + dummy + ")",
-  //     transform: "scale(" + dummy + ")",
-  //   });
-});
+/* -------------------------------------------------------------------------- */
+/*                                   UPDATE                                   */
+/* -------------------------------------------------------------------------- */
+
+function update() {
+  requestAnimationFrame(update);
+  renderer.render(scene, camera);
+  //   effect.render(scene, camera);
+}
+
+initScene();
+update();
